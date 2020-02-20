2f2745c09b29acba560511425306d66f672868cd
mpkness <mpkness@gmail.com>
2020-02-20 13:17:08 -0800

    changed external call from a hook to a promise, need to update tests for the change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React, {useState} from 'react';
 import StockDetails from './StockDetails.jsx';
+import {callStockAPI} from './hooks/callStockAPI.js';
 import './App.css';
 
 function App() {
   const [stocks, setStocks] = useState(['SNAP', 'MS', 'VZ', 'DNR', 'HPQ']);
   const [details, setDetails] = useState({symbol: '', name: '', price: '', stock_exchange: ''});
 
+  function fetchFromApi(stock) {
+    callStockAPI(stock)
+      .then(response => {
+        setDetails(response)
+      });
+  }
+
   function generateRows() {
     return stocks.map((stock, index) =>
       <tr key={index} className='stock-row'>
-        <td> {stock} </td>
+        <td onClick={() => {fetchFromApi(stock)}}> {stock} </td>
       </tr>
     );
   }
--- src/hooks/callStockAPI.js
+++ src/hooks/callStockAPI.js
@@ -1,28 +1,26 @@
-import {useEffect} from 'react';
 import axios from 'axios';
 import {stockApiUrl, stockApiToken} from '../constants.js';
 
 /**
  * given a stock name (the symbol), will fetch deatiled information on the stock from an external api
- * will then parse off only the information that will be used by the rest of the application and send
- * it to the given useState function
+ * will then parse off only the information that will be used by the rest of the application and return
+ * the parsed data
  * @param  {string} stockName     the stock name, symbol
- * @param  {function} updateState   updates the application state for stock details
- * @param  {object} [axios=axios] library for making an external call
- * @return {Null}
+ * @return {Promise} resolves with the response from the api
  */
-export const useStockAPI = (stockName, updateState, axios=axios) => {
+export const callStockAPI = (stockName) => {
   const fullStockUrl = stockApiUrl + '?symbol=' + stockName + '&api_token=' + stockApiToken;
-  useEffect(() => {
+  return new Promise((resolve, reject) => {
     axios.get(fullStockUrl)
       .then((response) => {
-        updateState(parseResponse(response.data[0])); // will always be first entry since always only requesting one stock
+        console.log(response);
+        resolve(parseResponse(response.data.data[0])); // will always be first entry since always only requesting one stock
       })
       .catch((error) => {
         console.log("ERROR", error);
+        resolve();
       });
-    });
-
+  });
 }
 
 /**
