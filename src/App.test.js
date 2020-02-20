25b748f810a1097210fc416282373481e6b59d9d
mpkness <mpkness@gmail.com>
2020-02-20 12:45:04 -0800

    add test for when a stock is clicked to update the details state

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,24 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
 
+const useStockAPIMock = jest.fn();
+const mockDetails = {
+  symbol: 'test symbol',
+  name: 'test name',
+  price: 'test price',
+  stock_exchange: 'test stock exchange'
+};
+useStockAPIMock.mockReturnValue(mockDetails);
+
 describe('App', () => {
   it('renders a table row for each stock in default state stock array', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('.stock-row').length).toBe(5);
+  });
+  it('should update details using the useStockAPI hook when a stock is clicked on', () => {
+    const wrapper = shallow(<App />);
+    wrapper.find('.stock-row').at(0).simulate('click');
+    const StockDetails = wrapper.find('StockDetails');
+    expect(StockDetails.props().details).toEqual(mockDetails);
   });
 });
