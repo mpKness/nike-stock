39c4d6fe8d1b319b8f511455a05bf5e53812b98d
mpkness <mpkness@gmail.com>
2020-02-20 10:47:38 -0800

    add enzyme and test for initial stock table

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders a table row for each stock in default state stock array', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.stock-row')).toBe(5);
 });
