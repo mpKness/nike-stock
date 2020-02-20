d5ea61a8ad52316d26afc5f660226d85492534aa
mpkness <mpkness@gmail.com>
2020-02-20 12:13:50 -0800

    add test for intial details section

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
 
-test('renders a table row for each stock in default state stock array', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find('.stock-row').length).toBe(5);
+describe('App', () => {
+  it('renders a table row for each stock in default state stock array', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.stock-row').length).toBe(5);
+  });
+  it('renders the details section with the empty default values', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.stock-detail').length).toBe(4);
+  });
 });
